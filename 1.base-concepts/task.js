1c862f94853ff000d6b8d4d928291a17a99e10e4
AnvarPro <anvarvahitov30@rambler.com>
2021-12-13 19:47:34 +0500

    FirstHWJS

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,18 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
-  return arr; // array
+  let x;
+    let D = b ** 2 - 4 * a * c;
+
+    if (D < 0) {
+      return [];
+    } else if (D === 0) {
+      return [-b / 2 * a];
+    } else {
+      return [(-b + Math.sqrt(D)) / (2 * a), (-b - Math.sqrt(D)) / (2 * a)];
+    }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
